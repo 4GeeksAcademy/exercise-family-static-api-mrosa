50dc686adf7425dc2b4c8c94cda1fc221f31cd1d
Mrosa90 <145835544+Mrosa90@users.noreply.github.com>
2024-04-15 08:17:45 +0000

    terminado

--- src/app.py
+++ src/app.py
@@ -6,21 +6,17 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
@@ -28,17 +24,34 @@ def sitemap():
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    if member is not None: 
 
-    return jsonify(response_body), 200
+        return jsonify({
+            'id': member['id'],
+            'first_name': member ['first_name'],
+            'age': member['age'],
+            'lucky_numbers': member['lucky_numbers']
+        }), 200
 
-# this only runs if `$ python src/app.py` is executed
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.get_json(silent = True)
+
+    jackson_family.add_member(body)
+    return jsonify({'done': True}), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    member = jackson_family.delete_member(id)
+
+    return jsonify({'done': True}), 200
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
